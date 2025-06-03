d3be621bfa0078d417ba249f77ca65c6dfd830ca
callerbalogun <callerbalogun@gmail.com>
2025-06-03 19:01:26 +0100

    bug fix

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -146,7 +146,8 @@ const OrderScreen = () => {
                       <Link to={`/product/${item.product}`}>{item.name}</Link>
                     </Col>
                     <Col md={4}>
-                      {item.qty} x ${item.price} = ${item.qty * item.price}
+                      {item.qty} x ${item.price} = $
+                      {(item.qty * item.price).toFixed(2)}
                     </Col>
                   </Row>
                 </ListGroup.Item>
